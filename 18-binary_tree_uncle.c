630a7ac30e951734ac5c6eb3382590c1c565e319
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-12-17 03:53:13 +0000

    aadded function definitions of helper functions

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,11 +8,27 @@
  * if the parent is NULL or if the node has no uncle
  */
 
-/* compile with 17-sibling.c */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
 
 	return (binary_tree_sibling(node->parent));
+}
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: pointer to the sibling node, NULL if node is NULL, if the parent is
+ * NULL or if the node has no siblings
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
